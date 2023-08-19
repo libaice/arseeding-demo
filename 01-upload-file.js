7c1bbdf6185748854477cd4a4c11886af6eb40cd
libaice <libaice147@gmail.com>
2023-08-19 15:36:03 +0800

    add upload file hash and upload txHash

--- 01-upload-file.js
+++ 01-upload-file.js
@@ -1,14 +1,13 @@
 const { genNodeAPI } = require("arseeding-js");
+require("dotenv").config();
 
-require('dotenv').config();
-const run = async () => {
-  const instance = genNodeAPI(
-    process.env.WALLET_PK
-  );
+const main = async () => {
+  const instance = genNodeAPI(process.env.WALLET_PK);
   const arseedUrl = "https://arseed.web3infra.dev";
-  const data = Buffer.from(" Hello A New Test From test/plain model ");
-  const payCurrencyTag =
-    "bsc-bnb-0x0000000000000000000000000000000000000000"; // everpay supported all token tag (chainType-symbol-id)
+  const data = Buffer.from(
+    " Hello This a new text to test url and explorer hash "
+  );
+  const payCurrencyTag = "bsc-bnb-0x0000000000000000000000000000000000000000"; // everpay supported all token tag (chainType-symbol-id)
   const options = {
     tags: [{ name: "Content-Type", value: "text/plain" }],
   };
@@ -18,6 +17,11 @@ const run = async () => {
     payCurrencyTag,
     options
   );
-  console.log("res", res);
+
+  console.log(
+    `uploaded url : https://arseed.web3infra.dev/${res.order.itemId}`
+  );
+  console.log(`upload txhash : https://scan.everpay.io/tx/${res.everHash} `);
 };
-run();
+
+main();
